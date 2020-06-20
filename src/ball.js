485a8133e7b952aa8bd764407c68baee418bf079
AliciaAMT <46018401+AliciaAMT@users.noreply.github.com>
2020-06-19 20:09:19 -0500

    Final

--- src/ball.js
+++ src/ball.js
@@ -1,14 +1,18 @@
+import { detectCollision } from "./collisionDetection";
+
 export default class Ball {
   constructor(game) {
     this.image = document.getElementById("img_ball");
     this.gameWidth = game.gameWidth;
     this.gameHeight = game.gameHeight;
-
     this.game = game;
-
-    this.position = { x: 10, y: 10 };
-    this.speed = { x: 4, y: 2 };
     this.size = 16;
+    this.reset();
+  }
+
+  reset() {
+    this.position = { x: 10, y: 400 };
+    this.speed = { x: 4, y: -2 };
   }
 
   draw(ctx) {
@@ -28,24 +32,15 @@ export default class Ball {
     if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
       this.speed.x = -this.speed.x;
     }
-    if (this.position.y + this.size > this.gameHeight || this.position.y < 0) {
+    if (this.position.y < 0) {
       this.speed.y = -this.speed.y;
     }
 
-    let bottomOfBall = this.position.y + this.size;
-
-    let topOfPaddle = this.game.paddle.position.y;
-
-    let leftSideOfPaddle = this.game.paddle.position.x;
-
-    let rightSideOfPaddle =
-      this.game.paddle.position.x + this.game.paddle.width;
-
-    if (
-      bottomOfBall >= topOfPaddle &&
-      this.position.x >= leftSideOfPaddle &&
-      this.position.x + this.size <= rightSideOfPaddle
-    ) {
+    if (this.position.y + this.size > this.gameHeight) {
+      this.game.lives--;
+      this.reset();
+    }
+    if (detectCollision(this, this.game.paddle)) {
       this.speed.y = -this.speed.y;
       this.position.y = this.game.paddle.position.y - this.size;
     }
